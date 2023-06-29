3bc6ad9d074d802a3016352f4e05bf0e2057ff3a
Andrey Rakhmatullin <wrar@wrar.name>
2023-06-29 17:10:45 +0400

    Add a test dataset for brands.

--- tests/test_brand.py
+++ tests/test_brand.py
@@ -1,9 +1,13 @@
+import json
+
+import pytest
 from lxml.html import fromstring
 
+from tests.utils import TEST_DATA_ROOT
 from zyte_parsers.brand import extract_brand_name
 
 
-def test_extract_brand():
+def test_extract_brand_simple():
     root = fromstring(
         '<div id="brand">simple brand</div>'
         '<div id="wrapper">'
@@ -23,3 +27,15 @@ def test_extract_brand():
     assert exa('//img[@id="img-bare"]') is None
     assert exa('//div[@id="wrapper"]') == "my alt brand"
     assert exa('//img[@id="img-long"]') == "short brand"
+
+
+@pytest.mark.parametrize(
+    "item",
+    json.loads((TEST_DATA_ROOT / "brand_values.json").read_text(encoding="utf8")),
+)
+def test_extract_brand(item):
+    if item.get("xfail"):
+        pytest.xfail(item["xfail"])
+
+    brand_name = extract_brand_name(fromstring(item["html"]), search_depth=2)
+    assert brand_name == item["value"]
